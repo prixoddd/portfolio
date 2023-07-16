2fbbc7d1fa8674789fbef47e53ee1ff2e512aa0f
nikita <nikita.derjugin@gmail.com>
2023-07-16 13:44:36 +0300

    + design + sass sent for audit

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -5,16 +5,18 @@ import styleContainer from '../common/styles/Container.module.sass';
 import Project from './project/Project';
 import Title from '../common/components/title/Title';
 
+const bImage = 'https://miro.medium.com/v2/resize:fit:0/0*KIKnUvzdIkp5zcDJ'
+
 const Projects = () => {
     return (
         <div className={`${s.projectsBlock}`}>
             <div className={`${styleContainer.container} ${s.projectsContainer}`}>
                 <Title title={'My Projects'} />
                 <div className={s.projects}>
-                    <Project projTitle='Social Network' projDescription='My social network is an app which we can scale and which can handle millions of data records.'/>
-                    <Project projTitle='Social Network' projDescription='My social network is an app which we can scale and which can handle millions of data records.'/>
-                    <Project projTitle='Counter' projDescription='My counter is functional'/>
-                    <Project projTitle='Counter' projDescription='My counter is functional'/>
+                    <Project backgroundImage={bImage} projTitle='Social Network' projDescription='My social network is an app which we can scale and which can handle millions of data records.'/>
+                    <Project backgroundImage={bImage} projTitle='Social Network' projDescription='My social network is an app which we can scale and which can handle millions of data records.'/>
+                    <Project backgroundImage={bImage} projTitle='Counter' projDescription='My counter is functional'/>
+                    <Project backgroundImage={bImage} projTitle='Counter' projDescription='My counter is functional'/>
                 </div>
             </div>
         </div>
--- src/projects/project/Project.tsx
+++ src/projects/project/Project.tsx
@@ -5,25 +5,30 @@ import e from '../../common/styles/Elements.module.sass'
 type ProjectPropsType = {
     projTitle: string
     projDescription: string
+    backgroundImage: string
 }
 
 const Project = (props: ProjectPropsType) => {
+
+    const divStyle = {
+        backgroundImage: `url("${props.backgroundImage}")`
+    }
+
     return (
         <div className={s.project}>
-            <div className={s.imageAndButton}>
-                {/*<img src="https://miro.medium.com/v2/resize:fit:0/0*KIKnUvzdIkp5zcDJ" alt=""/>*/}
+            <div className={s.imageAndButton} style={divStyle}>
                 <button className={e.elementButton}>
                     <span>Смотреть</span>
                 </button>
             </div>
-           <div className={s.projectInfo}>
-               <h3 className={s.projTitle}>
-                   {props.projTitle}
-               </h3>
-               <span className={s.projDescription}>
+            <div className={s.projectInfo}>
+                <h3 className={s.projTitle}>
+                    {props.projTitle}
+                </h3>
+                <span className={s.projDescription}>
                 {props.projDescription}
             </span>
-           </div>
+            </div>
 
         </div>
     );
