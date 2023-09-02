87bcbccd1c188265135ce02f7d7d7cc1ac993758
nikita <nikita.derjugin@gmail.com>
2023-09-02 15:34:29 +0300

    react-tilt removed because of the bug in chrome. react-parallax-tilt was installed instead of it.

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -5,7 +5,7 @@ import Particles from 'react-tsparticles'
 import type { Container, Engine } from 'tsparticles-engine'
 import { loadSlim } from 'tsparticles-slim'
 import ReactTypingEffect from 'react-typing-effect'
-import { Tilt } from 'react-tilt'
+import Tilt from 'react-parallax-tilt'
 
 const particlesOpt: any = {
     particles: {
@@ -117,20 +117,19 @@ const particlesOpt: any = {
     },
     retina_detect: true,
 }
+// const defaultOptions = {
+//     reverse: false, // reverse the tilt direction
+//     max: 35, // max tilt rotation (degrees)
+//     perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
+//     scale: 1, // 2 = 200%, 1.5 = 150%, etc..
+//     speed: 1000, // Speed of the enter/exit transition
+//     transition: true, // Set a transition on enter/exit.
+//     axis: null, // What axis should be disabled. Can be X or Y.
+//     reset: true, // If the tilt effect has to be reset on exit.
+//     easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
+// }
 
 const Main = () => {
-    const defaultOptions = {
-        reverse: false, // reverse the tilt direction
-        max: 35, // max tilt rotation (degrees)
-        perspective: 1000, // Transform perspective, the lower the more extreme the tilt gets.
-        scale: 1.1, // 2 = 200%, 1.5 = 150%, etc..
-        speed: 1000, // Speed of the enter/exit transition
-        transition: true, // Set a transition on enter/exit.
-        axis: null, // What axis should be disabled. Can be X or Y.
-        reset: true, // If the tilt effect has to be reset on exit.
-        easing: 'cubic-bezier(.03,.98,.52,.99)', // Easing on enter/exit.
-    }
-
     const particlesInit = useCallback(async (engine: Engine) => {
         console.log(engine)
 
@@ -165,7 +164,15 @@ const Main = () => {
                         <ReactTypingEffect text="A Frontend Developer" />
                     </div>
                 </div>
-                <Tilt options={defaultOptions}>
+                <Tilt
+                    className="tilt-img"
+                    tiltMaxAngleX={35}
+                    tiltMaxAngleY={35}
+                    perspective={900}
+                    scale={1.1}
+                    transitionSpeed={2000}
+                    gyroscope={true}
+                >
                     <div className={s.photo}></div>
                 </Tilt>
             </div>
