071d6c81866c054ed225c94f5280e861b1700d9e
nikita <nikita.derjugin@gmail.com>
2023-12-10 20:36:48 +0200

    refactoring 1

--- src/nav/Nav.tsx
+++ src/nav/Nav.tsx
@@ -4,21 +4,23 @@ import { Link } from 'react-scroll'
 
 const Nav = () => {
     return (
-        <div className={s.nav}>
-            {/*<a onClick={() => scroll.scrollTo(100)}>Main</a>*/}
-            <Link activeClass={s.active} to="main_block" spy={true} smooth={true} offset={50} duration={500}>
-                Main
-            </Link>
-            <Link activeClass={s.active} to="skills_block" spy={true} smooth={true} offset={50} duration={500}>
-                Skills
-            </Link>
-            <Link activeClass={s.active} to="projects_block" spy={true} smooth={true} offset={50} duration={500}>
-                Projects
-            </Link>
-            <Link activeClass={s.active} to="contacts_block" spy={true} smooth={true} offset={50} duration={500}>
-                Contacts
-            </Link>
-        </div>
+
+            <div className={s.nav}>
+                {/*<a onClick={() => scroll.scrollTo(100)}>Main</a>*/}
+                <Link activeClass={s.active} to="main_block" spy={true} smooth={true} offset={0} duration={500}>
+                    Main
+                </Link>
+                <Link activeClass={s.active} to="skills_block" spy={true} smooth={true} offset={50} duration={500}>
+                    Skills
+                </Link>
+                <Link activeClass={s.active} to="projects_block" spy={true} smooth={true} offset={50} duration={500}>
+                    Projects
+                </Link>
+                <Link activeClass={s.active} to="contacts_block" spy={true} smooth={true} offset={50} duration={500}>
+                    Contacts
+                </Link>
+            </div>
+
     )
 }
 
