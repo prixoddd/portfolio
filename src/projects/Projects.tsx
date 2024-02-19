73832cc02a4d4bfab2e79abd92b70ce77068ed41
Nikita <nikita.derjugin@gmail.com>
2024-02-19 11:45:36 +0200

    refactoring 2.2

--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -4,9 +4,10 @@ import { Zoom } from 'react-awesome-reveal'
 import counterBackground from 'img/projectsImages/counter.png'
 import todoList from 'img/projectsImages/todoList.png'
 import Project from './project/Project'
+import shipments from 'img/projectsImages/shipments.png'
 import Title from '../common/components/title/Title'
 
-const bImage = 'https://miro.medium.com/v2/resize:fit:0/0*KIKnUvzdIkp5zcDJ'
+import socialNetwork from 'img/projectsImages/socialNetwork.jpg'
 const Projects = () => {
     return (
         <div id="projects_block" className={`${s.projectsBlock}`}>
@@ -15,13 +16,19 @@ const Projects = () => {
                     <Title title={'My Projects'} />
                     <div className={s.projects}>
                         <Project
+                            backgroundImage={shipments}
+                            projTitle="Shipments CRUD"
+                            projDescription="An application built with TypeScript, React, and Redux, specifically designed for efficient CRUD operations. It utilizes Material-UI components along with custom-designed elements. Deployment is handled through Vercel."
+                            src="https://kn-hm.vercel.app/"
+                        />
+                        <Project
                             backgroundImage={todoList}
                             projTitle="Todo App"
                             projDescription="My social network is an app which we can scale and which can handle millions of data records."
                             src="https://prixoddd.github.io/todolist"
                         />
                         <Project
-                            backgroundImage={bImage}
+                            backgroundImage={socialNetwork}
                             projTitle="Social Network"
                             projDescription="My social network is an app which we can scale and which can handle millions of data records."
                             src="https://prixoddd.github.io/samurai-way-main"
